4cea1942fe12e4d3d43afb7dcb57692c73d03866
ricky <rpk2301@vt.edu>
2023-07-07 23:28:29 -0400

    In Progress

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
-//import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.HashMap;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,28 +13,50 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook =map;
     }
 
     public PhoneBook() {
-        this(null);
+        phonebook = new LinkedHashMap<>();
+
     }
 
     public void add(String name, String phoneNumber) {
+        //This line is going to make a list (just like the PhoneBook contstructor wants.
+        //Its going to look and say IF the name is present(as a key), return the values.
+        //otherwise. Create a new arraylist. Next we add phonenumber to either the
+        //new list OR the old list with the key. Put those numbers back into phonebook.
+        List<String> numbers = phonebook.getOrDefault(name,new ArrayList<>());
+        numbers.add(phoneNumber);
+        phonebook.put(name,numbers);
+
+
     }
 
-    public void addAll(String name, String... phoneNumbers) {
+    public void addAll(String name, String... phoneNumbers)
+    {
+        List<String> numbers = phonebook.getOrDefault(name,new ArrayList<>());
+        for(String x : phoneNumbers)
+        {
+            numbers.add(x);
+        }
+        phonebook.put(name,numbers);
     }
 
     public void remove(String name) {
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return phonebook.containsKey(name);
+    }
+
+    public Boolean hasEntry(String name, String number)
+    {
+        return phonebook.containsKey(name)&& phonebook.get(name).contains(number);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
@@ -45,10 +64,19 @@ public class PhoneBook {
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> ret = new ArrayList<>();
+        for(String x : phonebook.keySet())
+        {
+            ret.add(x);
+
+        }
+
+
+        return ret;
+
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
